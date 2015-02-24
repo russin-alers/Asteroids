6eaa53fab4f39c194d54f2c742505282287e2c8e
AsH <russin.alers@gmail.com>
2015-02-24 18:42:06 +0200

    Creating setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Asteroids',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/russin-alers/Asteroids',
+    license='',
+    author='AsH',
+    author_email='russin.alers@gmail.com',
+    description='2D game. livewares, tkinter'
+)
