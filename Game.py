1eac5562da6d333a2623f3dc041647f1f6db541d
AsH <russin.alers@gmail.com>
2015-02-25 11:12:01 +0200

    Adding missle, changing ship's movements rules.

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 from  livewires import games
-import random
+import random, math
 
 games.init(screen_height= 480,
            screen_width=640,
@@ -7,12 +7,12 @@ games.init(screen_height= 480,
 
 class Asteroid(games.Sprite):
     """Asteroid"""
-    ASTEROID_SIZE_SMALL = 1
-    ASTEROID_SIZE_MEDIUM = 2
-    ASTEROID_SIZE_LARGE = 3
-    images = {ASTEROID_SIZE_SMALL: games.load_image("icons/asteroid_small.bmp"),
-              ASTEROID_SIZE_MEDIUM: games.load_image("icons/asteroid_med.bmp"),
-              ASTEROID_SIZE_LARGE: games.load_image("icons/asteroid_big.bmp")}
+    SIZE_SMALL = 1
+    SIZE_MEDIUM = 2
+    SIZE_LARGE = 3
+    images = {SIZE_SMALL: games.load_image("icons/asteroid_small.bmp"),
+              SIZE_MEDIUM: games.load_image("icons/asteroid_med.bmp"),
+              SIZE_LARGE: games.load_image("icons/asteroid_big.bmp")}
     SPEED = 2
 
     def __init__(self,x,y,size):
@@ -34,27 +34,52 @@ class Asteroid(games.Sprite):
         if self.right < 0:
             self.left = games.screen.width
 
+class Missile(games.Sprite):
+    image = games.load_image("icons/missile.bmp")
+    sound = games.load_sound("sounds/missile.wav")
+    LIFETIME = 40
+    BUFFER = 40
+    M_ACCELERATION = 7
+
+    def __init__(self, ship_x, ship_y, ship_angle):
+
+        Missile.sound.play()
+        angle = ship_angle * math.pi / 180
+
+        #missile's starting position depends on ship's x,y and angle
+        buffer_x = Missile.BUFFER * math.sin(angle)
+        buffer_y = Missile.BUFFER * -math.cos(angle)
+        x = ship_x + buffer_x
+        y = ship_y + buffer_y
+
+        #missile's speed
+        dx = Missile.M_ACCELERATION * math.sin(angle)
+        dy = Missile.M_ACCELERATION * -math.cos(angle)
+
+        super(Missile, self).__init__(image=Missile.image,
+                                      x=x, y=y,
+                                      dx=dx, dy=dy,
+                                      angle = ship_angle)
+
+        self.lifetime = Missile.LIFETIME
+
+    def update(self):
+
+        self.lifetime -=1
+        #if self.lifetime == 0:
+            #self.destrroy()
 
 
 class Ship(games.Sprite):
     """Spaceship"""
-
-    ship_image = games.load_image("icons/ship.bmp")
     SHIP_SPEED = 1
     SHIP_ROTATION_STEP = 2
-    move_sound = games.load_sound("icons/thrust.wav")
+    ACCELERTATION = .03
+    ship_image = games.load_image("icons/ship.bmp")
+    move_sound = games.load_sound("sounds/thrust.wav")
 
     def update(self):
         """Moves and rotation"""
-        if games.keyboard.is_pressed(games.K_w):
-            self.y -= Ship.SHIP_SPEED
-        if games.keyboard.is_pressed(games.K_s):
-            self.y += Ship.SHIP_SPEED
-        if games.keyboard.is_pressed(games.K_a):
-            self.x -= Ship.SHIP_SPEED
-        if games.keyboard.is_pressed(games.K_d):
-            self.x += Ship.SHIP_SPEED
-
         if self.top > games.screen.height:
             self.bottom = 0
         if self.bottom < 0:
@@ -64,13 +89,19 @@ class Ship(games.Sprite):
         if self.right < 0:
             self.left = games.screen.width
 
+        if games.keyboard.is_pressed(games.K_UP):
+            self.y -= 1
+            Ship.move_sound.play()
+            angle = self.angle * math.pi / 180
+            self.dx += Ship.ACCELERTATION * math.sin(angle)
+            self.dy += Ship.ACCELERTATION * -math.cos(angle)
         if games.keyboard.is_pressed(games.K_RIGHT):
             self.angle += Ship.SHIP_ROTATION_STEP
         if games.keyboard.is_pressed(games.K_LEFT):
             self.angle -= Ship.SHIP_ROTATION_STEP
-
-
-
+        if games.keyboard.is_pressed(games.K_SPACE):
+            new_missile =  Missile(self.x, self.y, self.angle)
+            games.screen.add(new_missile)
 
 def main():
     background = games.load_image("icons/background.jpg", transparent=False)
@@ -79,7 +110,7 @@ def main():
     for i in range(8):
         x = random.randrange(games.screen.width)
         y = random.randrange(games.screen.height)
-        size = random.choice([Asteroid.ASTEROID_SIZE_SMALL,Asteroid.ASTEROID_SIZE_MEDIUM,Asteroid.ASTEROID_SIZE_LARGE])
+        size = random.choice([Asteroid.SIZE_SMALL,Asteroid.SIZE_MEDIUM,Asteroid.SIZE_LARGE])
         new_asteroid = Asteroid(x = x,y = y,size = size)
         games.screen.add(new_asteroid)
 
